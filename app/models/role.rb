352a302d85f73e1c4c45c6b128666de2e74ae8db
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-09-28 11:49:40 -0300

    Add Role#default_role_id method with caching
    Fix Role factory

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
+  DEFAULT_ROLE = 'Developer'
+
   has_many :memberships, dependent: :nullify
 
   validates :name, presence: true, uniqueness: true
+
+  def self.default_role_id
+    Rails.cache.fetch('cached_default_role_id', expires_in: 1.hour) do
+      Role.find_by(name: DEFAULT_ROLE)&.id
+    end
+  end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -13,4 +13,27 @@ describe Role, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
   end
+
+  describe '#default_role_id' do
+    subject { described_class.default_role_id }
+
+    before do
+      # Need to clear the cached properties
+      Rails.cache.delete('cached_default_role_id')
+    end
+
+    context 'when there is a Developer role' do
+      let!(:role) { create(:role) }
+
+      it 'returns the developer role id' do
+        expect(subject).to eq role.id
+      end
+    end
+
+    context 'when there is not a Developer role' do
+      it 'returns the developer role id' do
+        expect(subject).to be_nil
+      end
+    end
+  end
 end
