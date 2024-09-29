b5a93414d96adeae6b77271be188ba3198013071
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-09-28 22:30:16 -0300

    Add comment regarding querying in Roles::Memberships endpoint

--- app/controllers/roles/memberships_controller.rb
+++ app/controllers/roles/memberships_controller.rb
@@ -4,6 +4,8 @@ module Roles
   class MembershipsController < ApplicationController
     # GET /roles/:role_id/memberships
     def index
+      # Using this allows for one query and
+      # always returns the Role's data even if no Membership exists
       @role = Role.includes(:memberships).find(params[:role_id])
       @memberships = @role.memberships
     end
